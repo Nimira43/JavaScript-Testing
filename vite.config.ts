54b2f103139242c579f101ab38e94675b6399c4f
Nimira43 <n.rabone72@aol.com>
2024-12-05 22:22:25 +0000

    Vite config done

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/final/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config/**',
+    ]
+  }
+})
