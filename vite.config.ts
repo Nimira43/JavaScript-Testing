6c4389474dda201f16ca2c61b63528a8346b0c6b
Nimira43 <n.rabone72@aol.com>
2024-12-05 22:51:04 +0000

    Updated index.html and brought in new favicon

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
+    setupFiles: './src/vitest.setup.ts',
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
