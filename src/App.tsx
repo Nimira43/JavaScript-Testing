9b6a0bd16c485a0134776752b9a15f912148dd9d
Nimira43 <n.rabone72@aol.com>
2024-12-06 04:28:30 +0000

    Tweaks to logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className='p-8'>
       <h1 className='font-semibold text-2xl'>React Testing Library</h1>
-      <p className='mt-4 text-[#333]'>by NimiraTech</p>
+      <p className='mt-4 text-[#333] logo'>NimiraTech</p>
     </div>
   )
 }
