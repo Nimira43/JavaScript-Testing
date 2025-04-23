e42203b833422f18f6eda6f032f0fc3701277a9f
Nimira43 <n.rabone72@aol.com>
2025-04-23 08:54:20 +0100

    Testing Tailwind - all good. However logo font is terrible. Replaced with Bad Script font.

--- rtl/src/App.tsx
+++ rtl/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>React Testing Library</h1>      
+    <div className='p-6 text-4xl bg-main'>
+      <h1 className='text-light logo'>React Testing Library</h1>      
     </div>
   )
 }
