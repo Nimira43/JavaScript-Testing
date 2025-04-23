c5ee6bbfa12c42255fc78cb1fa5f45461f5fbf69
Nimira43 <n.rabone72@aol.com>
2025-04-23 10:03:16 +0100

    Rolled back version of Vite because of Vite plugin issue. Updated vite.config.ts, tidy up elsewhere.

--- rtl/vite.config.ts
+++ rtl/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/vitest.setup.ts',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/final/**', 
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*',
+    ],
+  },
 })
